c6264b63d48f7087f1992429abab4e0c1bc3d154
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-06-30 18:04:50 +0200

    Read host config from haskell

--- tool/app/Main.hs
+++ tool/app/Main.hs
@@ -8,22 +8,36 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 
--- module Main where
-
 import Lib
 import HaskellTypes
+import HaskellTypesTest
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import System.IO
 import Control.Monad
 
--- main :: IO ()
--- main = do
---   _ <- unQ makeTypes
---   let x = $makeTypes
-
 $makeTypes
 
-deriving instance Show TestType
-deriving instance Show AType
-deriving instance Show BType
+deriving instance Show IPv4
+deriving instance Show IPv4WithPort
+deriving instance Show IPv4Network
+deriving instance Show IPv4NetworkBoolMapEntry
+deriving instance Show TextBoolMapEntry
+deriving instance Show CAName
+deriving instance Show Directory
+deriving instance Show HostName
+deriving instance Show PkiInfo
+deriving instance Show DNSConfig
+deriving instance Show IsLighthouseConfig
+deriving instance Show PunchyInfo
+deriving instance Show LocalAllowListInfo
+deriving instance Show LighthouseInfo
+deriving instance Show SSHDUsers
+deriving instance Show SSHDInfo
+deriving instance Show InterfaceInfo
+deriving instance Show ListenInfo
+deriving instance Show TunInfo
+deriving instance Show TunRoute
+deriving instance Show TunUnsafeRoute
+deriving instance Show LogInfo
+deriving instance Show Host
--- tool/src/HaskellTypes.hs
+++ tool/src/HaskellTypes.hs
@@ -7,12 +7,66 @@
 
 module HaskellTypes (makeTypes) where
 
+import Dhall.Marshal.Encode
 import Dhall.TH as TH
 import Language.Haskell.TH
 
-makeTypes :: Q [Dec]
-makeTypes = TH.makeHaskellTypes [
-    TH.SingleConstructor "TestType" "MakeTestType" "./dhall/TestType.dhall"
-  , TH.SingleConstructor "BType" "MakeBType" "./dhall/BType.dhall"
-  , TH.MultipleConstructors "AType" "./dhall/AType.dhall"
+typesFile :: Text
+typesFile = "../dhall/types.dhall"
+
+data TypeOfType = Union | Record
+
+data TypeInfo = TypeInfo { t :: TypeOfType, name :: Text }
+
+mkCode :: Text -> Text -> Text
+mkCode file name = ("(" :: Text) <> file <> (")." :: Text) <> name
+
+fromInfoToHaskellType :: Text ->TypeInfo -> HaskellType Text
+fromInfoToHaskellType file (TypeInfo { name = n, t = t' }) =
+  let mkName :: Text
+      mkName = ("Make" :: Text) <> n
+  in case t' of
+      Union -> TH.MultipleConstructors n (mkCode file n)
+      Record -> TH.SingleConstructor n mkName (mkCode file n)
+
+hostTypes :: [TypeInfo]
+hostTypes = [
+    TypeInfo { name = "IPv4", t = Record }
+  , TypeInfo { name = "IPv4WithPort", t = Record }
+  , TypeInfo { name = "IPv4Network", t = Record }
+  , TypeInfo { name = "IPv4NetworkBoolMapEntry", t = Record }
+  , TypeInfo { name = "TextBoolMapEntry", t = Record }
+  , TypeInfo { name = "CAName", t = Record }
+  , TypeInfo { name = "Directory", t = Record }
+  , TypeInfo { name = "HostName", t = Record }
+  , TypeInfo { name = "PkiInfo", t = Record }
+  , TypeInfo { name = "DNSConfig", t = Record }
+  , TypeInfo { name = "IsLighthouseConfig", t = Record }
+  , TypeInfo { name = "PunchyInfo", t = Record }
+  , TypeInfo { name = "LocalAllowListInfo", t = Record }
+  , TypeInfo { name = "LighthouseInfo", t = Record }
+  , TypeInfo { name = "SSHDUsers", t = Record }
+  , TypeInfo { name = "SSHDInfo", t = Record }
+  , TypeInfo { name = "InterfaceInfo", t = Record }
+  , TypeInfo { name = "ListenInfo", t = Record }
+  , TypeInfo { name = "TunInfo", t = Record }
+  , TypeInfo { name = "TunRoute", t = Record }
+  , TypeInfo { name = "TunUnsafeRoute", t = Record }
+  , TypeInfo { name = "LogInfo", t = Record }
+  , TypeInfo { name = "Host", t = Record }
   ]
+
+extraTypesList :: [HaskellType Text]
+extraTypesList = [
+    -- TH.SingleConstructor "IPv4" "MakeIPv4" "../dhall/types/host/IPv4.dhall"
+    -- , TH.SingleConstructor "IPv4WithPort" "MakeIPv4WithPort" "../dhall/types/host/IPv4WithPort.dhall"
+  ]
+
+hostTypesList :: [ HaskellType Text ]
+hostTypesList = map (fromInfoToHaskellType typesFile) hostTypes
+
+typesList :: [HaskellType Text]
+typesList = extraTypesList <> hostTypesList
+
+makeTypes :: Q [Dec]
+makeTypes = TH.makeHaskellTypes typesList
