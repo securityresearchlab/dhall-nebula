696321e9102a0a3b06ad45ade242f7df42b4dce5
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-07-01 15:53:53 +0200

    Working tool

--- tool/app/Main.hs
+++ tool/app/Main.hs
@@ -13,6 +13,7 @@ module Main where
 import TH
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+import System.Exit
 import System.IO
 import System.Process
 import Control.Monad
@@ -23,19 +24,32 @@ import Dhall.Marshal.Decode
 import qualified Data.ByteString as B
 import qualified Data.Text as T
 import Data.String as S
+import Data.List
 import System.Directory (createDirectoryIfMissing)
 
 dhallBaseDir :: String
 dhallBaseDir = "../dhall/" -- the final slash is essential for the dhallToYaml method
+
+nebulaPath :: String
+nebulaPath = "..\\..\\nebula-windows-amd64\\nebula.exe"
+
+nebulaCertPath :: String
+nebulaCertPath = "..\\..\\nebula-windows-amd64\\nebula-cert.exe"
+
+caKeyPath :: String
+caKeyPath = "..\\..\\nebula-windows-amd64\\ca.key"
+
+caCrtPath :: String
+caCrtPath = "..\\..\\nebula-windows-amd64\\ca.crt"
+
+genericConfigContent :: String
+genericConfigContent = "let config = ./network-description.dhall let nebula = ./package.dhall in  nebula.generateHostConfig config.network config."
 
 hostConfigFileName :: String -> String
 hostConfigFileName h = h <> "-config.dhall"
 
 configFilePath :: String -> String
 configFilePath = (<>) dhallBaseDir
-
-genericConfigContent :: String
-genericConfigContent = "let config = ./network-description.dhall let nebula = ./package.dhall in  nebula.generateHostConfig config.network config."
 
 generateYamlExpression :: String -> String
 generateYamlExpression = (<>) genericConfigContent
@@ -51,9 +65,21 @@ main = do
   putStrLn "Reading network configuration"
   network <- input auto (T.pack ("(" <> dhallBaseDir <> "network-description.dhall).network")) :: IO Network
   putStrLn "Configuration read, writing yaml files"
-  let names = Prelude.map (T.unpack . name) (hosts network)
-  Control.Monad.mapM writeYamlFile names
+  results <- Control.Monad.mapM (generateYamlFile network) (hosts network)
+  let prettyResults = Prelude.map (\(h, v) -> (T.unpack (name h)) <> " is " <> (if v then "" else "not ") <> "valid") results
+  Control.Monad.mapM putStrLn prettyResults
   putStrLn "Finished"
+
+generateYamlFile :: Network -> Host -> IO (Host, Bool)
+generateYamlFile network host = do
+  let node_name = T.unpack (name host)
+  writeYamlFile node_name
+  validity <- verifyYamlFile $ generateNodeDirectory node_name
+  if validity
+    then do
+      _ <- generateCertKey host network $ generateNodeDirectory node_name
+      pure (host, validity)
+    else pure (host, validity)
 
 writeYamlFile :: String -> IO ()
 writeYamlFile node_name = do
@@ -64,9 +90,34 @@ writeYamlFile node_name = do
   createDirectoryIfMissing True (generateNodeDirectory node_name)
   B.writeFile filePath yamlContent
 
+executeShellCommand :: String -> IO Bool
+executeShellCommand command = do
+  let process = (shell command) { std_out = CreatePipe }
+  (_, _, _, ph) <- createProcess process
+  result <- waitForProcess ph
+  pure $ case result of
+          ExitSuccess   -> True
+          ExitFailure _ -> False
 
 verifyYamlFile :: FilePath -> IO Bool
-verifyYamlFile name = undefined
+verifyYamlFile path = do
+  let command = nebulaPath <> " -test -config " <> path
+  executeShellCommand command
 
-generateCertKey :: Text -> IO ()
-generateCertKey name = undefined
+
+generateCertKey :: Host -> Network -> String -> IO Bool
+generateCertKey host network dir = do
+  let isHostInGroup = (\g -> elem host (group_hosts g)) :: Group -> Bool
+  let host_name = T.unpack $ name host
+  let groups_names = Prelude.map (T.unpack . group_name) $ Prelude.filter isHostInGroup (groups network)
+  let host_ip = ((show . ip) host) <> "/" <> show (ip_mask network)
+  let groupsOption = if null groups_names then "" else (foldl (<>) "-groups \"" (intersperse "," groups_names)) <> "\""
+  let command = nebulaCertPath
+                  <> " sign -ca-key " <> caKeyPath
+                  <> " -ca-crt " <> caCrtPath
+                  <> " -name \"" <> host_name
+                  <> "\" -ip \"" <> host_ip
+                  <> "\" " <> groupsOption
+                  <> " -out-key " <> dir <> host_name <> ".key"
+                  <> " -out-crt "  <> dir <> host_name <> ".crt"
+  executeShellCommand command
