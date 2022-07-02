5b9c30b9e512a3d946dc31514c25d3e3cb881390
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-07-02 10:40:48 +0200

    Add arguments, parallelize config generation

--- tool/app/Main.hs
+++ tool/app/Main.hs
@@ -17,6 +17,7 @@ import System.Exit
 import System.IO
 import System.Process
 import Control.Monad
+import Control.Monad.Parallel
 import Dhall
 import Dhall.TH
 import Dhall.Yaml as DY
@@ -26,30 +27,28 @@ import qualified Data.Text as T
 import Data.String as S
 import Data.List
 import System.Directory (createDirectoryIfMissing)
+import System.Environment
 
-dhallBaseDir :: String
-dhallBaseDir = "../dhall/" -- the final slash is essential for the dhallToYaml method
+-- dhallBaseDir :: String
+-- dhallBaseDir = "../dhall/" -- the final slash is essential for the dhallToYaml method
 
-nebulaPath :: String
-nebulaPath = "..\\..\\nebula-windows-amd64\\nebula.exe"
+-- nebulaPath :: String
+-- nebulaPath = "..\\..\\nebula-windows-amd64\\nebula.exe"
 
-nebulaCertPath :: String
-nebulaCertPath = "..\\..\\nebula-windows-amd64\\nebula-cert.exe"
+-- nebulaCertPath :: String
+-- nebulaCertPath = "..\\..\\nebula-windows-amd64\\nebula-cert.exe"
 
-caKeyPath :: String
-caKeyPath = "..\\..\\nebula-windows-amd64\\ca.key"
+-- caKeyPath :: String
+-- caKeyPath = "..\\..\\nebula-windows-amd64\\ca.key"
 
-caCrtPath :: String
-caCrtPath = "..\\..\\nebula-windows-amd64\\ca.crt"
+-- caCrtPath :: String
+-- caCrtPath = "..\\..\\nebula-windows-amd64\\ca.crt"
 
 genericConfigContent :: String
 genericConfigContent = "let config = ./network-description.dhall let nebula = ./package.dhall in  nebula.generateHostConfig config.network config."
 
 hostConfigFileName :: String -> String
 hostConfigFileName h = h <> "-config.dhall"
-
-configFilePath :: String -> String
-configFilePath = (<>) dhallBaseDir
 
 generateYamlExpression :: String -> String
 generateYamlExpression = (<>) genericConfigContent
@@ -62,27 +61,33 @@ generateYamlFilePath name = (generateNodeDirectory name) <> name <> ".yaml"
 
 main :: IO ()
 main = do
-  putStrLn "Reading network configuration"
-  network <- input auto (T.pack ("(" <> dhallBaseDir <> "network-description.dhall).network")) :: IO Network
-  putStrLn "Configuration read, writing yaml files"
-  results <- Control.Monad.mapM (generateYamlFile network) (hosts network)
-  let prettyResults = Prelude.map (\(h, v) -> (T.unpack (name h)) <> " is " <> (if v then "" else "not ") <> "valid") results
-  Control.Monad.mapM putStrLn prettyResults
-  putStrLn "Finished"
+  args <- getArgs
+  case args of
+    [dhallBaseDir, nebulaPath, nebulaCertPath, caKeyPath, caCrtPath] -> do
+      putStrLn "Reading network configuration"
+      network <- input auto (T.pack ("(" <> dhallBaseDir <> "network-description.dhall).network")) :: IO Network
+      putStrLn "Configuration read, writing yaml files"
+      let generateYamlFile = setupYamlGeneration dhallBaseDir nebulaPath nebulaCertPath caKeyPath caCrtPath
+      results <- Control.Monad.Parallel.mapM (generateYamlFile network) (hosts network)
+      let prettyResults = Prelude.map (\(h, v) -> (T.unpack (name h)) <> " is " <> (if v then "" else "not ") <> "valid") results
+      Control.Monad.mapM putStrLn prettyResults
+      putStrLn "Finished"
+    _ -> putStrLn "Wrong number of arguments"
 
-generateYamlFile :: Network -> Host -> IO (Host, Bool)
-generateYamlFile network host = do
-  let node_name = T.unpack (name host)
-  writeYamlFile node_name
-  validity <- verifyYamlFile $ generateNodeDirectory node_name
-  if validity
-    then do
-      _ <- generateCertKey host network $ generateNodeDirectory node_name
-      pure (host, validity)
-    else pure (host, validity)
+setupYamlGeneration :: String -> String -> String -> String -> String -> (Network -> Host -> IO (Host, Bool))
+setupYamlGeneration dhallBaseDir nebulaPath nebulaCertPath caKeyPath caCrtPath =
+  \network -> \host -> do
+    let node_name = T.unpack (name host)
+    writeYamlFile dhallBaseDir node_name
+    validity <- verifyYamlFile nebulaPath $ generateNodeDirectory node_name
+    if validity
+      then do
+        _ <- generateCertKey nebulaCertPath caCrtPath caKeyPath host network $ generateNodeDirectory node_name
+        pure (host, validity)
+      else pure (host, validity)
 
-writeYamlFile :: String -> IO ()
-writeYamlFile node_name = do
+writeYamlFile :: String -> String -> IO ()
+writeYamlFile dhallBaseDir node_name = do
   putStrLn ("Generating yaml configuration for " <> node_name)
   let dhallExpression = T.pack (generateYamlExpression node_name)
   yamlContent <- DY.dhallToYaml DY.defaultOptions (Just dhallBaseDir) dhallExpression
@@ -93,20 +98,21 @@ writeYamlFile node_name = do
 executeShellCommand :: String -> IO Bool
 executeShellCommand command = do
   let process = (shell command) { std_out = CreatePipe }
-  (_, _, _, ph) <- createProcess process
+  (_, Just hout, _, ph) <- createProcess_ "execute shell command" process
   result <- waitForProcess ph
+  hClose hout
   pure $ case result of
           ExitSuccess   -> True
           ExitFailure _ -> False
 
-verifyYamlFile :: FilePath -> IO Bool
-verifyYamlFile path = do
+verifyYamlFile :: String -> FilePath -> IO Bool
+verifyYamlFile nebulaPath path = do
   let command = nebulaPath <> " -test -config " <> path
   executeShellCommand command
 
 
-generateCertKey :: Host -> Network -> String -> IO Bool
-generateCertKey host network dir = do
+generateCertKey :: String -> String -> String -> Host -> Network -> String -> IO Bool
+generateCertKey nebulaCertPath caCrtPath caKeyPath host network dir = do
   let isHostInGroup = (\g -> elem host (group_hosts g)) :: Group -> Bool
   let host_name = T.unpack $ name host
   let groups_names = Prelude.map (T.unpack . group_name) $ Prelude.filter isHostInGroup (groups network)
