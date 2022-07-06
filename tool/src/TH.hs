c6ed9f381b9b48a51059a257eb03ea9eb89d3722
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-07-06 20:09:27 +0200

    Add Ord instance deriving for uniqueness

--- tool/src/TH.hs
+++ tool/src/TH.hs
@@ -19,93 +19,139 @@ instance Show IPv4 where
 
 deriving instance Eq IPv4
 
+deriving instance Ord IPv4
+
 deriving instance Show IPv4WithPort
 
 deriving instance Eq IPv4WithPort
+
+deriving instance Ord IPv4WithPort
 
 deriving instance Show IPv4Network
 
 deriving instance Eq IPv4Network
 
+deriving instance Ord IPv4Network
+
 deriving instance Show IPv4NetworkBoolMapEntry
 
 deriving instance Eq IPv4NetworkBoolMapEntry
+
+deriving instance Ord IPv4NetworkBoolMapEntry
 
 deriving instance Show TextBoolMapEntry
 
 deriving instance Eq TextBoolMapEntry
 
+deriving instance Ord TextBoolMapEntry
+
 deriving instance Show CAName
 
 deriving instance Eq CAName
+
+deriving instance Ord CAName
 
 deriving instance Show Directory
 
 deriving instance Eq Directory
 
+deriving instance Ord Directory
+
 deriving instance Show HostName
 
 deriving instance Eq HostName
+
+deriving instance Ord HostName
 
 deriving instance Show PkiInfo
 
 deriving instance Eq PkiInfo
 
+deriving instance Ord PkiInfo
+
 deriving instance Show DNSConfig
 
 deriving instance Eq DNSConfig
+
+deriving instance Ord DNSConfig
 
 deriving instance Show IsLighthouseConfig
 
 deriving instance Eq IsLighthouseConfig
 
+deriving instance Ord IsLighthouseConfig
+
 deriving instance Show PunchyInfo
 
 deriving instance Eq PunchyInfo
+
+deriving instance Ord PunchyInfo
 
 deriving instance Show LocalAllowListInfo
 
 deriving instance Eq LocalAllowListInfo
 
+deriving instance Ord LocalAllowListInfo
+
 deriving instance Show LighthouseInfo
 
 deriving instance Eq LighthouseInfo
+
+deriving instance Ord LighthouseInfo
 
 deriving instance Show SSHDUsers
 
 deriving instance Eq SSHDUsers
 
+deriving instance Ord SSHDUsers
+
 deriving instance Show SSHDInfo
 
 deriving instance Eq SSHDInfo
+
+deriving instance Ord SSHDInfo
 
 deriving instance Show InterfaceInfo
 
 deriving instance Eq InterfaceInfo
 
+deriving instance Ord InterfaceInfo
+
 deriving instance Show ListenInfo
 
 deriving instance Eq ListenInfo
+
+deriving instance Ord ListenInfo
 
 deriving instance Show TunInfo
 
 deriving instance Eq TunInfo
 
+deriving instance Ord TunInfo
+
 deriving instance Show TunRoute
 
 deriving instance Eq TunRoute
+
+deriving instance Ord TunRoute
 
 deriving instance Show TunUnsafeRoute
 
 deriving instance Eq TunUnsafeRoute
 
+deriving instance Ord TunUnsafeRoute
+
 deriving instance Show LogInfo
 
 deriving instance Eq LogInfo
 
+deriving instance Ord LogInfo
+
 deriving instance Show Host
 
 deriving instance Eq Host
+
+deriving instance Ord Host
 
 deriving instance Show GroupName
 
