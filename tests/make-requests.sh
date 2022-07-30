1a3d423abd14452981e9de779ab66ff3d512ec61
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-07-30 15:14:59 +0200

    Change curl output

--- tests/make-requests.sh
+++ tests/make-requests.sh
@@ -14,7 +14,7 @@ while [[ "$?" == 28 ]]; do echo "waiting for nebula to connect"; sleep 20; curl 
 echo "starting requests"
 start=`date +%s`
 while [[ $((`date +%s` - $start)) -le $(( $2 * 60)) ]]; do
-    curl -w "%{response_code};%{time_total}\n" -o /dev/null -s --max-time 10 http://192.168.0.2:8080 >> $NAME.txt &
+    echo -n `date +%s`';' >> $NAME.txt && curl -w "%{response_code};%{time_total}\n" -o /dev/null -s --max-time 10 http://192.168.0.2:8080 >> $NAME.txt &
     sleep 0.25s
 done
 
