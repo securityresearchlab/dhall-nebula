0943ec6750865785d4a82d6e2e7d2e1d4c16d624
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-07-30 15:50:55 +0200

    Make requests destination configurable; requests 1.8

--- tests/make-requests.sh
+++ tests/make-requests.sh
@@ -8,17 +8,17 @@ echo nebula ip _._.$N_IP3.$N_IP4
 NAME=${N_IP3}${N_IP4}
 
 echo "now waiting for nebula to connect"
-curl --max-time 10 http://192.168.0.2:8080
-while [[ "$?" == 28 ]]; do echo "waiting for nebula to connect"; sleep 20; curl --max-time 10 http://192.168.0.2:8080; done
+curl --max-time 10 http://$3:8080
+while [[ "$?" == 28 ]]; do echo "waiting for nebula to connect"; sleep 20; curl --max-time 10 http://$3:8080; done
 
 echo "starting requests"
 start=`date +%s`
 while [[ $((`date +%s` - $start)) -le $(( $2 * 60)) ]]; do
-    echo -n `date +%s`';' >> $NAME.txt && curl -w "%{response_code};%{time_total}\n" -o /dev/null -s --max-time 10 http://192.168.0.2:8080 >> $NAME.txt &
+    echo -n `date +%s`';' >> $NAME.txt && curl -w "%{response_code};%{time_total}\n" -o /dev/null -s --max-time 10 http://$3:8080 >> $NAME.txt &
     sleep 0.25s
 done
 
 wait $!
 sleep 30
 echo "upload results"
-curl -F "file=@$NAME.txt" -X POST http://192.168.0.2:8090/$NAME
+curl -F "file=@$NAME.txt" -X POST http://$3:8090/$NAME
