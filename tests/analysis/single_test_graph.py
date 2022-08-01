72912770c50c2bdddc3bca191aa551bb1a318abf
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-08-01 10:58:58 +0200

    Graph for both nebula and non nebula tests

--- tests/analysis/single_test_graph.py
+++ tests/analysis/single_test_graph.py
@@ -3,14 +3,27 @@ import numpy as np
 import pandas as pd
 import sys
 
-df = pd.read_csv(sys.argv[1], sep=';', header=None, names=['date', 'code', 'time'], on_bad_lines='skip', index_col=False).dropna()
+def img(df, maxT, title, name):
+	mean = [np.mean(df['time'])]*df.shape[0]
+	_, ax = plt.subplots()
+	plt.title(title)
+	ax.set_ylim([0, maxT])
+	ax.scatter(df['date'], df['time'], marker='o', s=3)
+	ax.plot(df['date'], mean, label='Media', linestyle='--', color='red')
+	plt.ylabel('time (s)')
+	plt.xlabel('timestamp')
+	plt.savefig(name + ".png")
 
-good = df[df['code'] != 0]
-bad = df[df['code'] == 0]
+nameWN = "all" + sys.argv[1] + ".txt"
+nameNN = "all" + sys.argv[1] + "nn.txt"
 
-df.plot(x ='date', y='time', kind = 'scatter', use_index = False, title='Risultati test con ' + sys.argv[2] + ' client')
+dfWN = pd.read_csv(nameWN, sep=';', header=None, names=['date', 'code', 'time'], on_bad_lines='skip', index_col=False).dropna()
+dfNN = pd.read_csv(nameNN, sep=';', header=None, names=['date', 'code', 'time'], on_bad_lines='skip', index_col=False).dropna()
 
-plt.ylabel('time (s)')
-plt.xlabel('timestamp')
+maxT = max(dfWN['time'].max(), dfNN['time'].max())
 
-plt.savefig(sys.argv[1] + ".png")
+goodWN = dfWN[dfWN['code'] != 0]
+badWN = dfWN[dfWN['code'] == 0]
+
+img(dfWN, maxT, 'Risultati test con ' + sys.argv[1] + ' client', nameWN)
+img(dfNN, maxT, 'Risultati test senza Nebula con ' + sys.argv[1] + ' client', nameNN)
